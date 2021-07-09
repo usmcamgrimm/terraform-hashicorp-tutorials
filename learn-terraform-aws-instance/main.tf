283c9fbdf2bd1bcd61fcf7142fa32a2dfea8bb97
Adrian Grimm <usmcamgrimm@gmail.com>
2021-07-09 18:51:47 -0400

    Storing Remote State
    Migrating state to Terraform Cloud
    
    1. Updated main.tf
    2. Create an account on Terraform Cloud
    3. In terminal, run $ terraform login
    4. Copy the access token or use stored creds
    $ terraform init : will re-initialize teh config and migrates the state file
    5. Delete the state file(s)
    6. Set workspace variables for workspace created in main.tf
    7. For this, I used my AWS keys. Set both to Sensitive and save
    8. $ terraform apply : terraform will not detect any differences, state is now monitored and storted remotely.
    9. $ terraform destroy ; this will clean uiup resources created ion the tutorial

--- learn-terraform-aws-instance/main.tf
+++ learn-terraform-aws-instance/main.tf
@@ -1,4 +1,10 @@
 terraform {
+  backend "remote" {
+    organization = "grimm-terraform-learning"
+    workspaces {
+      name = "learning-terraform-grimm-us"
+    }
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
