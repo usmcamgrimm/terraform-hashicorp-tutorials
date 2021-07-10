92875d5d8c6f6194484f8eda8bae4809ed5cc3eb
Adrian Grimm <usmcamgrimm@gmail.com>
2021-07-10 13:53:03 -0400

    Migrate state to Terraform Cloud

---
+++ learn-terraform-azure-instance/example.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~> 2.65"
+    }
+  }
+
+  backend "remote" {
+    organization = "grimm-terraform-learning"
+    workspaces {
+      name = "Example-Workspace-Azure"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name = "myTFResourceGroup"
+  location = "eastus2"
+}
--- learn-terraform-azure-instance/main.tf
+++ learn-terraform-azure-instance/main.tf
@@ -1,33 +1,33 @@
 # Configure the Azure provider
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 2.65"
-    }
-  }
+#terraform {
+#  required_providers {
+#    azurerm = {
+#      source  = "hashicorp/azurerm"
+#      version = "~> 2.65"
+#    }
+#  }
+#
+#  required_version = ">= 0.14.9"
+#}
 
-  required_version = ">= 0.14.9"
-}
+#provider "azurerm" {
+#  features {}
+#}
 
-provider "azurerm" {
-  features {}
-}
+#resource "azurerm_resource_group" "rg" {
+#  name     = var.resource_group_name
+#  location = "eastus2"
 
-resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = "eastus2"
-
-  tags = {
-    Environment = "Terraform Getting Started"
-    Team = "DevOps"
-  }
-}
+#  tags = {
+#    Environment = "Terraform Getting Started"
+#    Team = "DevOps"
+#  }
+#}
 
 # Create a virtual network
-resource "azurerm_virtual_network" "vnet" {
-  name                = "myTFVnet"
-  address_space       = ["10.0.0.0/16"]
-  location            = "eastus2"
-  resource_group_name = azurerm_resource_group.rg.name
-}
+#resource "azurerm_virtual_network" "vnet" {
+#  name                = "myTFVnet"
+#  address_space       = ["10.0.0.0/16"]
+#  location            = "eastus2"
+#  resource_group_name = azurerm_resource_group.rg.name
+#}
