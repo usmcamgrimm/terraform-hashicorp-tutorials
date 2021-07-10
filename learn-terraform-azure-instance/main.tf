e7ea1d50d89339e769e26aba4f54d480fb56ea2b
Adrian Grimm <usmcamgrimm@gmail.com>
2021-07-10 12:46:15 -0400

    Create a New Resource
    Virtual Network - VNet
    
    1. Update main.tf resource block
    2. Apply changes: $ terraform apply

--- learn-terraform-azure-instance/main.tf
+++ learn-terraform-azure-instance/main.tf
@@ -18,3 +18,11 @@ resource "azurerm_resource_group" "rg" {
   name     = "myTFResourceGroup"
   location = "eastus2"
 }
+
+# Create a virtual network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "myTFVnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = "eastus2"
+  resource_group_name = azurerm_resource_group.rg.name
+}
